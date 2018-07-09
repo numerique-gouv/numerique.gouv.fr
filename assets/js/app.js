77b986dedd201a0061c1e6f30cac80741131c257
ETBE <ebernoux@octo.com>
2018-07-09 16:29:32 +0200

    add hamburger animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,16 @@ $(document).foundation();
 //
 // Custom JS
 // --------------------------------------------------
+
+var button = document.querySelector('#hamburger-button');
+var activatedClass = 'is-active';
+
+button.addEventListener('click', function(e) {
+  e.preventDefault();
+  if(button.classList.contains(activatedClass)){
+    button.classList.remove(activatedClass);
+
+  } else {
+    button.classList.add(activatedClass);
+  }
+});
