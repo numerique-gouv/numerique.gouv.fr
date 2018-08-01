5862e8a0b7c0182a606fea8011abe3d2d10ab382
ETBE <ebernoux@octo.com>
2018-08-01 17:34:17 +0200

    update ci

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,9 +11,8 @@ import jQuery from 'jquery'
 window.jQuery = jQuery;
 window.$ = jQuery;
 import whatInput from 'what-input';
-
-
 import Foundation from 'foundation-sites';
+
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
@@ -23,6 +22,7 @@ import libs from './lib/dependancies';
 window.libs = libs;
 
 $(document).foundation();
+
 
 libs.AOS.init();
 
