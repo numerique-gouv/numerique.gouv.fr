2a84612859928573f1522e5c342304f4657d1eb3
ETBE <ebernoux@octo.com>
2018-09-17 17:05:31 +0200

    change path

--- scripts/import-avis-conforme.py
+++ scripts/import-avis-conforme.py
@@ -21,9 +21,9 @@ for ministere in ministeres:
 
     res_titre = ministere.h4.text
     url_image=ministere.p.img['src']
-    res_image =  "../assets/img/avis-conformes/"+url_image.split("/").pop()
+    res_image =  "/assets/img/avis-conformes/"+url_image.split("/").pop()
     requete = requests.get(base_url+url_image, stream=True)
-    file_image = open(res_image, 'w')
+    file_image = open(".."+res_image, 'w')
     file_image.write(requete.content)
     file_image.close()
     annees = ministere.findChildren("div" , recursive=False)
@@ -35,17 +35,17 @@ for ministere in ministeres:
         res_avis = []
         for avi in avis:
             url_pdf = avi.a['href']
-            res_pdf =  "../_uploads/"+url_pdf.split("/").pop()
+            res_pdf = url_pdf.split("/").pop()
             requete = requests.get(base_url+url_pdf, stream=True)
-            file_pdf = open(res_pdf, 'w')
+            file_pdf = open("../_uploads/"+res_pdf, 'w')
             file_pdf.write(requete.content)
             file_pdf.close()
             res_nom,res_text = avi.a.text.split("(", 1)
             res_text = res_text[:-1]
             res_avi = {
-              "nom":res_nom,
-              "pdf":res_pdf,
-              "text":res_text
+              "titre":res_nom,
+              "pdf":"/uploads/"+res_pdf,
+              "description":res_text
             }
             res_avis.append(res_avi)
 
