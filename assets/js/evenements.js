efd0398adf2071bc2136e34ca67f5a370db046ac
ETBE <ebernoux@octo.com>
2018-09-21 16:48:08 +0200

    some change on event

--- assets/js/evenements.js
+++ assets/js/evenements.js
@@ -3,7 +3,7 @@ import instantsearch from "instantsearch.js";
 import { configure, menuSelect, refinementList, infiniteHits } from "instantsearch.js/es/widgets";
 import { connectRange } from "instantsearch.js/es/connectors"
 
-import { collection_routing_conf } from "./conf/routing-conf"
+import { agenda_routing_conf } from "./conf/routing-conf"
 import { Instantsearch_factory } from "./instant-search/instantsearch-factory";
 import { Instantsearch_builder } from "./instant-search/instantsearch-builder";
 import { configureConf, menuSelectConf, refinementListConf, infiniteHitsConfEvent } from "./conf/wiggetConf"
@@ -11,36 +11,46 @@ import { configureConf, menuSelectConf, refinementListConf, infiniteHitsConfEven
 
 const searchClient = algoliasearch('OCGRURLBFM','4acb079286ac50d2c359cdc0bf0af4d7');
 
-const search = new Instantsearch_factory(instantsearch,searchClient,collection_routing_conf).init();
+const search = new Instantsearch_factory(instantsearch,searchClient,agenda_routing_conf).init();
 const instantsearch_builder = new Instantsearch_builder(search);
 
-const datePicker = instantsearch.connectors.connectRange(
+const datePicker = connectRange(
   (options, isFirstRendering) => {
     if (!isFirstRendering) return;
-
+    console.log(options)
+    const start_date = moment();
+    const end_date = moment().add(1, 'months');
+    const earliest_date = moment("2018-01-01");
+    const latest_date = moment().add(2, 'years').endOf('year');
     const { refine } = options;
+    refine ([start_date.unix(), end_date.unix()]);
 
     new Calendar({
       element: $('#calendar'),
       callback: function() {
-        const start = new Date(this.start_date).getTime();
-        const end = new Date(this.end_date).getTime();
-
-        refine([start, end]);
+        let start = moment(this.start_date),
+          end = moment(this.end_date);
+        refine([start.unix(), end.unix()]);
       },
       // Some good parameters based on our dataset:
-      start_date: new Date(),
-      end_date: new Date('01/01/2020'),
-      earliest_date: new Date('01/01/2008'),
-      latest_date: new Date('01/01/2020'),
+      start_date: start_date,
+      end_date: end_date,
+      earliest_date: earliest_date,
+      latest_date: latest_date,
+      same_day_range: true,
+      format: {
+        input: 'D MMMM YYYY', // Format for the input fields
+        jump_month: 'MMMM', // Format for the month switcher
+        jump_year: 'YYYY' // Format for the year switcher
+      },
+      presets: false
     });
   }
 );
 
-instantsearch.addWidget(
-  datePicker({
+instantsearch_builder.addWidget(datePicker, {
     attributeName: 'date',
-  })
+  }
 );
 
 instantsearch_builder.addWidget(configure,configureConf("événements"));
