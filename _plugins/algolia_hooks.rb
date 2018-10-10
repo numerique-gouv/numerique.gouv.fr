f389c6cf9be08d32bc3f2a0c2d1289810c5f0702
lucien mollard <mollard.lucien@gmail.com>
2018-10-10 18:01:35 +0200

    update plugin for algolia

--- _plugins/algolia_hooks.rb
+++ _plugins/algolia_hooks.rb
@@ -2,8 +2,11 @@ module Jekyll
   module Algolia
     module Hooks
       def self.before_indexing_each(record, node, context)
-        if record[:event_date] != nil
-          record[:event_date] = Date.parse(record[:event_date]).to_time.to_i
+        if record[:event_start_date] != nil
+          record[:event_start_date] = Date.parse(record[:event_start_date]).to_time.to_i
+        end
+        if record[:event_end_date] != nil
+          record[:event_end_date] = Date.parse(record[:event_end_date]).to_time.to_i
         end
         record
       end
