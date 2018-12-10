f3587fd05ff5b12735f36375e73082d54e119ed7
lucien mollard <mollard.lucien@gmail.com>
2018-12-10 16:08:09 +0100

    add params to req

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,16 +3,14 @@ import axios from "axios"
 $('#contact-form').submit(function(event) {
   event.preventDefault();
 
-  const data = {
-    from: $("input[name='from']").val(),
-    name: $("input[name='name']").val(),
-    subject: $("input[name='subject']").val(),
-    category: $("select[name='category']").val(),
-    text: $("textarea[name='text']").val()
-  }
-
   axios.post('https://dev.numerique.gouv.fr/api/mail', {
-    params: data
+    params: {
+      from: $("input[name='from']").val(),
+      name: $("input[name='name']").val(),
+      subject: $("input[name='subject']").val(),
+      category: $("select[name='category']").val(),
+      text: $("textarea[name='text']").val()
+    }
   })
     .then(function (response) {
       console.log(response);
