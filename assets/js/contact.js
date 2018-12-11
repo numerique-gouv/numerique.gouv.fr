28a31e88c323bd3279bfc6795cf4c9d050819d22
lucien mollard <mollard.lucien@gmail.com>
2018-12-11 10:44:14 +0100

    add message for contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -11,11 +11,15 @@ $('#contact-form').submit(function(event) {
     text: $("textarea[name='text']").val()
   }
 
+  console.log(process.env)
+
   axios.post('https://dev.numerique.gouv.fr/api/mail', params)
     .then(function (response) {
-      console.log(response);
+      $('#message').removeClass('hide').addClass('green-message').html('Votre message à bien été envoyé')
+      $('input').val('')
+      $('textarea').val('')
     })
     .catch(function (error) {
-      console.log(error);
+      $('#message').removeClass('hide').addClass('red-message')
     });
 });
