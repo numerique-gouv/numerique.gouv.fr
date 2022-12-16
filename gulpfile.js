373d77f8adcc3448ac4d59a0b2175e5444390d21
Lucien <mollard.lucien@gmail.com>
2022-12-16 12:12:32 +0100

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,16 @@ const sass = require('gulp-sass')(require('sass'));
 // Require all tasks in gulp/tasks, including subfolders
 requireDir('./gulp/tasks', { recurse: true });
 
+function buildStyles() {
+  return gulp.src('../../sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+};
+
+exports.buildStyles = buildStyles;
+exports.watch = function () {
+  gulp.watch('../../sass/**/*.scss', ['sass']);
+};
 
 gulp.task('build',
   gulp.series('clean', 'jekyll-build', 'modernizr',
