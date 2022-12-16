4af36637b48c14c0bc10df36894d30d8fe74c9d2
Lucien <mollard.lucien@gmail.com>
2022-12-16 12:17:25 +0100

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,32 @@ const sass = require('gulp-sass')(require('sass'));
 // Require all tasks in gulp/tasks, including subfolders
 requireDir('./gulp/tasks', { recurse: true });
 
-function buildStyles() {
-  return gulp.src('../../sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-};
+const browser       = require ("browser-sync");
+const yargs         = require ('yargs');
+const $             = require('gulp-load-plugins')();
 
-exports.buildStyles = buildStyles;
-exports.watch = function () {
-  gulp.watch('../../sass/**/*.scss', ['sass']);
-};
+const COMPATIBILITY = require('./gulp/util/loadConfig').COMPATIBILITY;
+const SASS          = require('./gulp/util/loadConfig').SASS;
+const PRODUCTION = !!(yargs.argv.production);
+
+gulp.task('sass', function() {
+  return gulp.src(SASS.src)
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+      includePaths: SASS.include
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: COMPATIBILITY
+    }))
+    // Comment in the pipe below to run UnCSS in production
+    //.pipe($.if(PRODUCTION, $.uncss(UNCSS_OPTIONS)))
+    .pipe($.if(PRODUCTION, $.cleanCss({ compatibility: 'ie9' })))
+    .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
+    .pipe(gulp.dest(SASS.dest.jekyllRoot))
+    .pipe(gulp.dest(SASS.dest.buildDir))
+    .pipe(browser.reload({ stream: true }));
+});
 
 gulp.task('build',
   gulp.series('clean', 'jekyll-build', 'modernizr',
